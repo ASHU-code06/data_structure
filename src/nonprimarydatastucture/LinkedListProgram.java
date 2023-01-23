8acf9c879eeb235251b7d90f5f4b637f4ab03ecb
Ashuhauhan <ashuc306@gmail.com>
2023-01-24 05:14:57 +0600

    Linked List

--- src/nonprimarydatastucture/LinkedListProgram.java
+++ src/nonprimarydatastucture/LinkedListProgram.java
@@ -1,48 +1,15 @@
 package nonprimarydatastucture;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class LinkedListProgram {
-     class Node {
-        int data;
-        Node next;
-        Node(int _data) {
-            this.data = _data;
-            this.next = null;
-        }
-    }
-     Node head=null;
-     public void creation(){
-         int n=0;
-         do{
-         System.out.println("Enter data");
-         Scanner input=new Scanner(System.in);
-         int data= input.nextInt();
-         Node new_node=new Node(data);
-         if (head==null){
-           head=new_node;
-         }else {
-             new_node.next=head;
-             head=new_node;
-         }
-         System.out.println("Do you want to enter more data if yes then press 1,if no then press 2");
-         n= input.nextInt();
-         }while(n==1);
-     }
-     public void traverse(){
-         Node temp=head;
-         if(temp==null){
-             System.out.println("Linked list does not exist");
-         }else {
-             System.out.println("Linked list is  ");
-             while (temp!=null){
-             System.out.println(temp.data);
-             temp=temp.next;}
-         }
-     }
     public static void main(String[] args){
-        LinkedListProgram linkedListProgram=new LinkedListProgram();
-        linkedListProgram.creation();
-        linkedListProgram.traverse();
+        LinkedList<Integer> al= new LinkedList<>();
+        al.addFirst(56);
+        al.add(30);
+        al.add(70);
+        System.out.println("Linked list is ");
+         System.out.println(al);
     }
 
 
